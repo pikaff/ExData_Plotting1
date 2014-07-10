552ca4a612ebb6f51f709dcf968cd76de5d7dd33
pikaff <pikaff@users.noreply.github.com>
2014-07-09 21:41:56 -0400

    fix plot 4
    remove legend border

--- plot4.R
+++ plot4.R
@@ -18,7 +18,7 @@ with(mydata, lines(time, Sub_metering_2, col="red"))
 with(mydata, lines(time, Sub_metering_3, col="blue"))
 
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       col=c("black","red","blue"), lty=1)
+       col=c("black","red","blue"), lty=1, bty = "n")
 
 #top right
 with(mydata, plot(time, Voltage, type="l",  xlab="datetime"))
